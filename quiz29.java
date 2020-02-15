b3c2f872581d8bd3e33cf2bdaa7a63a895f6cca9
ROHANI <rohani1996@gmail.com>
2020-02-15 11:34:20 -0500

    Add files via upload

---
+++ quiz29.java
@@ -0,0 +1,51 @@
+/*
+ * public class SimpleCalculator {
+  private double firstNumber;
+    private double secondNumber;
+ 
+    public double getFirstNumber() {
+        return firstNumber;
+    }
+ 
+    public double getSecondNumber(){
+        return secondNumber;
+    }
+ 
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+ 
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
+    }
+ 
+    public double getAdditionResult(){
+        return firstNumber+secondNumber;
+    }
+ 
+    public double getSubtractionResult() {
+        return firstNumber-secondNumber;
+    }
+ 
+    public double getMultiplicationResult() {
+        return firstNumber*secondNumber;
+    }
+ 
+    public double getDivisionResult() {
+        if (secondNumber == 0) {
+            return 0;
+        } return firstNumber/secondNumber;
+    }
+}
+ */
+
+package sectionSix;
+
+public class quiz29 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
