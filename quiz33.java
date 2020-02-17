ce9b4b227aab5561cbe741ab2b5a3d8a9fa36894
ROHANI <rohani1996@gmail.com>
2020-02-16 19:28:18 -0500

    Add files via upload

--- quiz33.java
+++ quiz33.java
@@ -1,5 +1,55 @@
 package sectionSix;
 
-public class quiz33 {
+public class quiz33 //class floor
+{
+	private double length;
+	private double width;
+	
+	public quiz33()
+	{
+		//return(0,0); 
+	}
+	
+	public quiz33(double width, double length)
+	{
+		if(width < 0) this.width = 0; 
+		if(length < 0) this.length = 0; 
+		this.width = width;
+		this.length = length; 
+	}
+	
+	public double getLength() 
+	{
+		return length;
+	}
+	public void setLength(double length) 
+	{
+		this.length = length;
+	}
+	public double getWidth() 
+	{
+		return width;
+	}
+	public void setWidth(double width) 
+	{
+		this.width = width;
+	} 
+	
+	public double getArea()
+	{
+		return this.width * this.length; 
+	}
+	
+
 
 }
+
+
+
+
+
+
+
+
+
+
