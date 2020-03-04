b3d02f48094e620f6890373105d790ed77cbdfba
ROHANI <rohani1996@gmail.com>
2020-03-03 23:03:51 -0500

    Add files via upload

--- OOPBankAccChallenge/src/BankAccount.java
+++ OOPBankAccChallenge/src/BankAccount.java
@@ -1,12 +1,37 @@
 
 public class BankAccount 
 {
-	private int number;
+	private String number;
 	private int balance;
 	private String customerName;
 	private String email; 
 	private int phoneNumber;
 	
+	public BankAccount() // a constructor. used to intialize the obj u are using (BankAccount bobsAccount = new BankAccount();)
+	{
+		this("56789", 2, "default name", "default email", 1234455); 
+		//^^ these are the paramters used when creating this obj if u create an obj from this class and dont ger any parameters
+		System.out.println("empty constructor called");
+	}
+	public BankAccount(String number, int balance, String customerName, String email, int phoneNumber)
+	{
+		System.out.println("Account constructors called");
+		this.number = number;
+		this.balance = balance;
+		this.customerName = customerName; 
+		this.email = email;
+		this.phoneNumber = phoneNumber; 
+		//what this is doing is setting the field values in the constructor. 
+	}
+	
+	public BankAccount(String number, String customerName, String email, int phoneNumber) 
+	{
+		this("99999", 100, customerName, email, phoneNumber);
+		this.number = number;
+		this.email = email;
+		this.phoneNumber = phoneNumber;
+		this.customerName = customerName;
+	}
 	public void depositFunds(double depositAmount)
 	{
 		balance += depositAmount; 
@@ -24,11 +49,11 @@ public class BankAccount
 			System.out.println("Withdrawl of " + withdrawalAmount + " Processed " + balance + " left");
 		}
 	}
-	public int getNumber() 
+	public String getNumber() 
 	{
 		return number;
 	}
-	public void setNumber(int number)
+	public void setNumber(String number)
 	{
 		this.number = number;
 	}
--- OOPBankAccChallenge/src/Main.java
+++ OOPBankAccChallenge/src/Main.java
@@ -4,11 +4,15 @@ public class Main
 
 	public static void main(String[] args) 
 	{
-		BankAccount bobsAccount = new BankAccount();
+		BankAccount bobsAccount = new BankAccount("1223", 0, "bob", "email@gmail", 12345678);
+		bobsAccount.setNumber("12344");
+		bobsAccount.setBalance(0);
 		bobsAccount.withdrawFunds(100.0);
 		
 		bobsAccount.depositFunds(50.0);
 		bobsAccount.withdrawFunds(51.0); 
+		
+		//BankAccount timsAccount = new BankAccount("1234", "tim" "tim@gmail", 1234);
 	}
 
 }
